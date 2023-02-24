ee333c7f909b2ea56f8fedbab5c755f9e3c034bf
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-02-24 19:31:11 +0530

    Create Pattern 4.cpp

---
+++ Pattern 4.cpp
@@ -0,0 +1,9 @@
+void printTriangle(int n) {
+        // code here
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=i;j++){
+                printf("%d ",i);
+            }
+            printf("\n");
+        }
+    }
