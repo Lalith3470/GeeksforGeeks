cdcae1ca051335bbb86e55c694f64b440d5a649f
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-03-01 23:27:26 +0530

    Create Search in a matrix.py

---
+++ Search in a matrix.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+	def matSearch(self,mat, N, M, X):
+		# Complete this function
+		for i in mat:
+		    if x in i:
+		        return 1
+		return 0
