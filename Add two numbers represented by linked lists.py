9fa13a7c8a78136579b747b3deec5e1cf3d8c84b
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-03-11 22:20:58 +0530

    Create Add two numbers represented by linked lists.py

---
+++ Add two numbers represented by linked lists.py
@@ -0,0 +1,20 @@
+class Solution:
+    #Function to add two numbers represented by linked list.
+    def addTwoLists(self, first, second):
+        # code here
+        # return head of sum list
+        s1=""
+        s2=""
+        while first:
+            s1+=str(first.data)
+            first=first.next
+        while second:
+            s2+=str(second.data)
+            second=second.next
+        ans=str(int(s1)+int(s2))
+        a=Node(0)
+        tmp=a
+        for i in ans:
+            tmp.next=Node(int(i))
+            tmp=tmp.next
+        return a.next
