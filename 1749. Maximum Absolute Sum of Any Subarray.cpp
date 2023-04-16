7421efbc62aabd6acc858b6ad461ee9f473af9ba
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-04-16 22:30:18 +0530

    Create 1749. Maximum Absolute Sum of Any Subarray.cpp

---
+++ 1749. Maximum Absolute Sum of Any Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int mx=0,sm=0;
+        for(auto i:nums){
+            sm+=i;
+            if(sm<0) sm=0;
+            if(sm>mx) mx=sm;
+        }
+        int min=0,sm1=0;
+        for(auto i:nums){
+            sm1+=i;
+            if(sm1<min) min=sm1;
+            if(sm1>0) sm1=0;
+        }
+        return max(mx,abs(min));
+    }
+};
