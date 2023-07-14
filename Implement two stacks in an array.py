04715712e61d881aa62b72f669bf0a2fd1bb38c7
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-07-14 23:14:07 +0530

    Create Implement two stacks in an array.py

---
+++ Implement two stacks in an array.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+class TwoStacks:
+    def __init__(self, n=100):
+        self.size = n
+        self.arr = [0] * n
+        self.top1 = -1
+        self.top2 = n
+
+    # Function to push an integer into stack 1
+    def push1(self, x):
+        self.top1 += 1
+        self.arr[self.top1] = x
+
+    # Function to push an integer into stack 2
+    def push2(self, x):
+        self.top2 -= 1
+        self.arr[self.top2] = x
+
+    # Function to remove an element from top of stack 1
+    def pop1(self):
+        if self.top1 == -1:
+            return -1
+        self.top1 -= 1
+        return self.arr[self.top1 + 1]
+
+    # Function to remove an element from top of stack 2
+    def pop2(self):
+        if self.top2 == self.size:
+            return -1
+        self.top2 += 1
+        return self.arr[self.top2 - 1]
