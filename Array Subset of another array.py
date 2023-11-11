43deaa761d420375eb723de984d43adbbb1dd0bd
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-11-11 20:47:13 +0530

    Update Array Subset of another array.py

--- Array Subset of another array.py
+++ Array Subset of another array.py
@@ -3,7 +3,6 @@ from collections import Counter
 def isSubset( a1, a2, n, m):
     cnt=Counter(a1)
     cnt2=Counter(a2)
-    
     for i,j in cnt2.items():
         if i not in cnt:
             return "No"
